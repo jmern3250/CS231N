25eb1df9e34cfca5214266bb6de18787190dbbbc
jmern3250 <jmern91@stanford.edu>
2017-04-19 16:51:34 -0700

    Completed softmax

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -1,6 +1,7 @@
 import numpy as np
 from random import shuffle
 from past.builtins import xrange
+import pdb 
 
 def softmax_loss_naive(W, X, y, reg):
   """
@@ -24,16 +25,30 @@ def softmax_loss_naive(W, X, y, reg):
   loss = 0.0
   dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+  N = X.shape[0]
+  D = X.shape[1]
+  C = W.shape[1]
+
+  for i in range(N):
+    S = X[i].dot(W)
+    S -= np.max(S)
+    eS = np.zeros([C,1])
+    for j, s in enumerate(S): 
+      eS[j] = np.exp(s)
+    eS_sum = np.sum(eS)
+    for j, s in enumerate(S):
+      dW[:,j] += np.exp(s)/eS_sum*X[i]
+    ey = np.exp(S[y[i]])
+    loss -= np.sum(np.log(ey/eS_sum))
+    dW[:, y[i]] -= X[i]
+
+  loss /= N 
+
+  dW /= N
+
+  loss += reg*np.sum(W*W)
+
+  dW += reg*2*W
 
   return loss, dW
 
@@ -48,16 +63,23 @@ def softmax_loss_vectorized(W, X, y, reg):
   loss = 0.0
   dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+  N = X.shape[0]
+  D = X.shape[1]
+  C = W.shape[1]
 
+  S = X.dot(W)
+  S -= np.matrix(np.max(S, axis=1)).T
+    
+  loss -= S[np.arange(N), y]
+  S_sum = np.sum(np.exp(S), axis=1)
+  loss += np.log(S_sum)
+  loss = np.sum(loss)
+  loss /= N 
+  loss += reg*np.sum(W*W)
+  
+  Cf = np.exp(S)/np.matrix(S_sum).T
+  Cf[np.arange(N),y] -= 1
+  dW = X.T.dot(Cf)
+  dW /= N
+  dW += reg*2*W
   return loss, dW
-
